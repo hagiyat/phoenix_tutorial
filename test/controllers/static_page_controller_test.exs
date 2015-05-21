d496703d9c4278ed6cdbdeb2978ab633e9e929aa
hagiyat <hagiyat@gmail.com>
2015-05-22 00:37:20 +0900

    added generaly contents search function for test.

--- test/controllers/static_page_controller_test.exs
+++ test/controllers/static_page_controller_test.exs
@@ -3,16 +3,21 @@ defmodule RailsTutorial.StaticPageControllerTest do
 
   test "GET /static_page/home" do
     conn = get conn(), "/static_page/home"
-    assert html_response(conn, 200) =~ "StaticPage#home"
+    assert find_contents(conn, "title") == {["Hello Phoenix!"]}
+    assert find_contents(conn, "h1") == {["StaticPage#home"]}
   end
 
   test "GET /static_page/help" do
     conn = get conn(), "/static_page/help"
-    assert html_response(conn, 200) =~ "StaticPage#help"
+    assert conn.status == 200
+    assert find_contents(conn, "title") == {["Hello Phoenix!"]}
+    assert find_contents(conn, "h1") ==  {["StaticPage#help"]}
   end
 
   test "GET /static_page/about" do
     conn = get conn(), "/static_page/about"
-    assert html_response(conn, 200) =~ "About Us"
+    assert conn.status == 200
+    assert find_contents(conn, "title") == {["Hello Phoenix!"]}
+    assert find_contents(conn, "h1") ==  {["About Us"]}
   end
 end
